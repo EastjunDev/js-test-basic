aeb5db609d6432725c1de2097ba46c8b4133a8e8
Ki Tae Park <rlxo1651@daum.net>
2020-10-09 16:35:47 +0900

    Counter 단위 테스트 미션 구현(val, inc, dec, isMax, isMin, with Option & without Option)

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,22 @@
-export function createCounter() {
+export function createCounter(initValue) {
   return {
+    value: initValue?.value ? initValue.value : 0,
+    min: initValue?.min ? initValue.min : -Infinity,
+    max: initValue?.max ? initValue.max : Infinity,
     val() {
-
+      return this.value;
     },
     inc() {
-
+      return this.value === this.max ? this.value : ++this.value;
     },
     dec() {
-
+      return this.value === this.min ? this.value : --this.value;
+    },
+    isMax() {
+      return this.value === this.max;
+    },
+    isMin() {
+      return this.value === this.min;
     }
   };
 }
