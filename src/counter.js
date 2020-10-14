71206cb3fcdd60d4da69281fb94c81dec7936e3b
ykpark <read0more@gmail.com>
2020-10-14 22:18:12 +0900

    1주차 미션 작성

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,31 @@
-export function createCounter() {
+export function createCounter(initObj = {}) {  
+  const { initVal, initMin, initMax } = initObj;
+  let value = initVal || 0;
+  const min = typeof initMin === 'number' ? initMin : -Infinity;
+  const max = typeof initMax === 'number' ? initMax : Infinity;
+
   return {
     val() {
-
+      return value;
     },
     inc() {
-
+      if (!this.isMax()) {
+        value++;
+      }            
     },
     dec() {
-
+      if (!this.isMin()) {
+        value--;
+      }      
+    },
+    isMax() {
+      return value === max;
+    },
+    isMin() {
+      return value === min;
+    },
+    min() {
+      return min;
     }
   };
 }
