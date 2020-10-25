4c0143f81bd30eed7b2d90d58ae1a89fbee80302
lsw1164 <qkqkfo@gmail.com>
2020-10-26 01:11:11 +0900

    Implement TodoList & TodoItem

--- src/todoApp/Components/TodoList.js
+++ src/todoApp/Components/TodoList.js
@@ -1,9 +1,9 @@
 import { isValidTodoList } from "../utils/validate.js";
+import TodoItem from "./TodoItem.js";
 export default function TodoList(props) {
   if (!isValidTodoList(props?.todoList)) {
     return "";
   }
-  console.log(props.todoList);
-  return `
-  `;
+  const { todoList } = props;
+  return todoList.map((todoItem) => TodoItem(todoItem)).join(" ");
 }
