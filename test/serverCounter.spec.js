9d50aabe02886fc07022d4dce13af3a60b9e52ec
ykpark <read0more@gmail.com>
2020-10-30 14:45:30 +0900

    servercounter 실습 완료

--- test/serverCounter.spec.js
+++ test/serverCounter.spec.js
@@ -3,29 +3,35 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { getByText, fireEvent, getByTestId, waitFor, findByTestId } from '@testing-library/dom';
 import { createServerCounter } from '../src/serverCounter/counter';
+import { createCounter } from "../src/backup/counter";
 
+let $container;
 const mockAxios = new MockAdapter(axios);
-
-let container;
-const INIT_VALUE = 10
-const MIN_VALUE = 8
-const MAX_VALUE = 12
-
-beforeEach(async () => {
-  container = document.createElement('div');
-  
-  mockAxios.onGet('/counter').reply(200, {
-    value: INIT_VALUE,
+const createServerCounterMock = ($el, options) => {
+  const counter = createCounter(options);    
+  const getMockResponse = () => ({
+    value: counter.val(),
+    isMax: counter?.isMax(),
+    isMin: counter?.isMin()
   });
-  mockAxios.onPut('/counter/dec').reply(200, {
-    value: INIT_VALUE-1,
-  });        
-  mockAxios.onPut('/counter/inc').reply(200, {
-    value: INIT_VALUE+1,    
-  });      
 
-  createServerCounter(container);  
-  document.body.appendChild(container);
+  mockAxios.onGet('/counter').reply(200, getMockResponse());
+  mockAxios.onPut('/counter/dec').reply(() => {
+    counter.dec();
+    const mockResponse = getMockResponse();    
+    return [200, mockResponse];
+  });
+  mockAxios.onPut('/counter/inc').reply(() => {
+    counter.inc();    
+    const mockResponse = getMockResponse();        
+    return [200, mockResponse];
+  });
+  createServerCounter($el);      
+};
+
+beforeEach(() => {
+  $container = document.createElement('div');
+  document.body.appendChild($container);
 });
 
 afterEach(() => {
@@ -33,40 +39,69 @@ afterEach(() => {
   mockAxios.reset();
 });
 
-it('생성시 버튼과 초기값을 렌더링한다.', async () => {
-  expect(getByText(container, '+')).toBeVisible();
-   expect(getByText(container, '-')).toBeVisible();
+it('생성시 버튼과 초기값을 렌더링한다.', async () => {  
+   const INIT_VALUE = 10;
+   createServerCounterMock($container, { initVal: INIT_VALUE });
+   const $incBtn = await waitFor(() => getByText($container, "+"));
+   const $decBtn = await waitFor(() => getByText($container, "-"));
    
-   expect(getByText(container, `${INIT_VALUE}`)).toBeInTheDocument();
+   expect($incBtn).toBeVisible();
+   expect($decBtn).toBeVisible();
    
-   expect(getByText(container, '+')).toHaveClass('btn-inc');
-   expect(getByText(container, '-')).toHaveClass('btn-dec');
+   expect(getByText($container, `${INIT_VALUE}`)).toBeInTheDocument();
    
-   expect(getByText(container, '+')).not.toBeDisabled();
-   expect(getByText(container, '-')).not.toBeDisabled();
+   expect($incBtn).toHaveClass('btn-inc');
+   expect($decBtn).toHaveClass('btn-dec');
+   
+   expect($incBtn).not.toBeDisabled();
+   expect($decBtn).not.toBeDisabled();
       
-   expect(getByTestId(container, 'value')).toHaveTextContent(INIT_VALUE);
+   expect(getByTestId($container, 'value')).toHaveTextContent(INIT_VALUE);
 });
 
-it('+ 버튼 클릭시 서버에 inc요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {    
-    fireEvent.click(getByText(container, '+'));
-    await waitFor(() => { expect(getByTestId(container, 'value')).toHaveTextContent(INIT_VALUE+1); });
+it('+ 버튼 클릭시 서버에 inc요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {
+    const INIT_VALUE = 10;
+    createServerCounterMock($container, { initVal: INIT_VALUE });
+    const $incBtn = await waitFor(() => getByText($container, "+"));
+    fireEvent.click($incBtn);
+    fireEvent.click($incBtn);
+    await waitFor(() => { expect(getByTestId($container, 'value')).toHaveTextContent(INIT_VALUE+2); });
 });
 
 it('- 버튼 클릭시 서버에 dec 요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {
-  fireEvent.click(getByText(container, '-'));
-  await waitFor(() => { expect(getByTestId(container, 'value')).toHaveTextContent(INIT_VALUE-1); });  
+    const INIT_VALUE = 7;
+    createServerCounterMock($container, { initVal: INIT_VALUE });
+    const $decBtn = await waitFor(() => getByText($container, "-"));
+    fireEvent.click($decBtn);
+    fireEvent.click($decBtn);
+
+    await waitFor(() => { expect(getByTestId($container, 'value')).toHaveTextContent(INIT_VALUE-2); });
 });
 
 it('최대값이면 + 버튼이 disabled 상태가 되고, 클릭해도 서버에 요청을 보내지 않는다', async () => {
-  fireEvent.click(getByText(container, '+'));
-  fireEvent.click(getByText(container, '+'));
-  fireEvent.click(getByText(container, '+'));
+  const INIT_VALUE = 10;
+  const MAX_VALUE = 11;
+  createServerCounterMock($container, { initVal: INIT_VALUE, max: MAX_VALUE });
 
-  expect(getByText(container, '+')).toBeDisabled();
-  expect(getByTestId(container, 'value')).toHaveTextContent(MAX_VALUE);
+  let $incBtn = await waitFor(() => getByText($container, "+"));
+
+  fireEvent.click($incBtn);
+  fireEvent.click($incBtn);
+
+  await waitFor(() => expect($incBtn).toHaveProperty("disabled"));
+  await waitFor(() => { expect(getByTestId($container, 'value')).toHaveTextContent(MAX_VALUE); });
 });
 
 it('최소값이면 - 버튼이 disabled 상태가 되고, 클릭해도 서버에 요청을 보내지 않는다', async () => {
+  const INIT_VALUE = 3;
+  const MIN_VALUE = 2;
+  createServerCounterMock($container, { initVal: INIT_VALUE, min: MIN_VALUE });
 
+  let $decBtn = await waitFor(() => getByText($container, "-"));
+
+  fireEvent.click($decBtn);
+  fireEvent.click($decBtn);
+
+  await waitFor(() => expect($decBtn).toHaveProperty("disabled"));
+  await waitFor(() => { expect(getByTestId($container, 'value')).toHaveTextContent(MIN_VALUE); });
 });
